0782341110e81324944759575083692bfdcbae3d
Dreaith <andrea.cunanan@neu.edu.ph>
2025-01-04 21:26:16 +0800

    Add files via upload

---
+++ fundamentalsofp/brocode/Mad_Libs_Game1.java
@@ -0,0 +1,42 @@
+package brocode;
+
+import java.util.Scanner;
+
+public class Mad_Libs_Game1 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String adjective1;
+		String noun1;
+		String adjective2;
+		String verb1;
+		String adjective3;
+		
+		
+		System.out.println("===========Welcome User! This is Mad Libs Game============");
+		System.out.println();
+		System.out.println("Enter an adjective : ");
+		adjective1=sc.nextLine();
+		System.out.println("Enter a noun : ");
+		noun1=sc.nextLine();
+		System.out.println("Enter a adjective :");
+		adjective2=sc.nextLine();
+		System.out.println("Enter a verb (action) that ends with -ing:");
+		verb1=sc.nextLine();
+		System.out.println("Enter a adjective :");
+		adjective3=sc.nextLine();
+		
+		
+
+		System.out.println();
+		System.out.println("Today i went to" + adjective1 + " place.");
+		System.out.println("At the entrance,I suddenly saw "+ noun1);
+		System.out.println(noun1 + " was " + adjective2 + verb1);
+		System.out.println("I am very  "+adjective3);
+	
+	}
+
+}
---
+++ fundamentalsofp/brocode/variable1.java
@@ -0,0 +1,25 @@
+package brocode;
+
+public class variable1 {
+
+	public static void main(String[] args) {
+		//variable = reusable container for a value
+		
+		//2 types:
+		//primitive = simply value stored directly in memory (stack)
+		//reference = memory 
+		
+		
+		//2 steps to create a variable: declaration and assignment
+		//declaration {ex:}
+		//int age;
+	
+		//assignment {ex:}
+		int age = 21;
+		int year=2024;
+		int quantity = 1;
+		
+		System.out.println( age + year + quantity );
+	}
+
+}
