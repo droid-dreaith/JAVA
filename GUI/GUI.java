abd5e42b4a48230cf62a650ae696462cf8c2d30e
Dreaith <andrea.cunanan@neu.edu.ph>
2025-04-09 22:15:32 +0800

    gui

---
+++ GUI/GUI.java
@@ -0,0 +1,107 @@
+package Gui;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class GUI implements ActionListener{
+
+    private int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
+	
+	
+	public GUI() {
+		createFrame();
+		createButton();
+		createLabel();
+		addComponentsToFrame();
+
+	}
+
+	private void createFrame() {
+		frame = new JFrame("Our GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+	private void createButton() {
+		JButton button = new JButton("Click me");
+		button.addActionListener(this);
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+	}
+
+	private void createLabel() {
+		label = new JLabel("Number of clicks: 0");
+		panel.add(label);
+	}
+
+	private void addComponentsToFrame() {
+		frame.add(panel, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+	}		
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number of clicks: " + count);
+	}
+
+	
+	public static void main(String[] args) {
+		new GUI(); 
+
+	}
+}
+
+
+
+
+		/*
+
+		JFrame frame = new JFrame();
+		
+		JButton button = new JButton("Click me");
+		button.addActionListener(this);
+		JLabel label = new JLabel("Number of clicks: 0");
+		
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Our GUI");
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new GUI(); 
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number of clicks: " + count);
+	}
+
+}
+ */ 
+		
---
+++ GUI/firstGui.java
@@ -0,0 +1,20 @@
+
+import javax.swing.JOptionPane;
+
+
+public class firstGui {
+
+	public static void main(String[] args) {
+		
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hello" + name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));	
+		JOptionPane.showMessageDialog(null,"You are " + age + " years old");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter height"));
+		JOptionPane.showMessageDialog(null,"Youa are " + height + " cm ");
+	} 
+				
+
+}
